e990d5cc0293d3e2c57d9c35d5bbb77f265c7152
BukenyaAbdusalaam <105359998+Ssekyanzi-Joel@users.noreply.github.com>
2024-08-28 11:02:34 +0300

    filecoverter

--- fileconverter/views.py
+++ fileconverter/views.py
@@ -1,41 +1,3 @@
-# import pandas as pd
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from .forms import UploadFileForm
-
-# def format_row(row):
-    # date_field = str(int(row['Date'])) if not pd.isna(row['Date']) else '0'.ljust(18)
-    # code_field = str(int(row['Account Code'])) if not pd.isna(row['Account Code']) else '0'.ljust(7)
-    # amount_field = str(int(row['Amount'])) if not pd.isna(row['Amount']) else '0'.rjust(42)
-    # currency_field = str(row['Currency']) if not pd.isna(row['Currency']) else '0'.rjust(4)
-    
-#     date_field = date_field.ljust(18)
-#     code_field = code_field.ljust(7)
-#     amount_field = amount_field.rjust(42)
-#     currency_field = currency_field.rjust(4)
-    
-#     return f"{date_field}{code_field}{amount_field} {currency_field}"
-
-# def upload_file(request):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # Read the Excel file
-#             excel_file = request.FILES['file']
-#             df = pd.read_excel(excel_file)
-            
-#             # Format the rows
-#             formatted_rows = "\n".join(df.apply(format_row, axis=1))
-            
-#             # Create an HTTP response with the formatted text file
-#             response = HttpResponse(formatted_rows, content_type='text/plain')
-#             response['Content-Disposition'] = 'attachment; filename="formatted_output.txt"'
-#             return response
-#     else:
-#         form = UploadFileForm()
-    
-#     return render(request, 'upload.html', {'form': form})
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
@@ -49,6 +11,7 @@ class UploadFileForm(forms.Form):
 
 
 def format_row(row):
+    free_space = ' ' * 10 
     date_field = str(int(row['Date'])) if not pd.isna(row['Date']) else '0'.ljust(18)
     code_field = str(int(row['Account Code'])) if not pd.isna(row['Account Code']) else '0'.ljust(7)
     amount_field = str(int(row['Amount'])) if not pd.isna(row['Amount']) else '0'.rjust(42)
@@ -59,7 +22,7 @@ def format_row(row):
     amount_field = amount_field.rjust(42)
     currency_field = currency_field.rjust(4)
     
-    return f"{date_field}{code_field}{amount_field} {currency_field}"
+    return f"{free_space}{date_field}{code_field}{amount_field} {currency_field}"
 
 
 # Function to process Excel file and generate txt files
@@ -69,7 +32,7 @@ def generate_txt_files(excel_file):
     txt_files = {}
     
     for sheet_name in xls.sheet_names:
-        df = pd.read_excel(xls, sheet_name=sheet_name)
+        df = pd.read_excel(xls, sheet_name=sheet_name, skiprows=4)
 
         # Replace NaN values with 0
         df.fillna(0, inplace=True)
